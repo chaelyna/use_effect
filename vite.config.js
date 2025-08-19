7f6e0cb707e00cdd62a4877ff26bc3f0404c72fa
chaelyna <rlflsdl32@naver.com>
2025-08-19 11:57:35 +0900

    config base 변경

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-project-sample/', // 여기에 실제 저장소 이름 입력
+  base: '/use_effect/', // 여기에 실제 저장소 이름 입력
   build: {
     outDir: 'dist'
   }
