b9ac06953842d9e4d8460ea52f63658965eedda6
Runa <runa.uxlabs@gmail.com>
2025-08-19 11:15:11 +0900

    주석 변경

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 
-// 정리 작업은 리소스를 해제하거나 이벤트를 제거하는 데 사용
 function Timer() {
   const [time, setTime] = useState(0);
 
@@ -13,7 +12,8 @@ function Timer() {
     console.log("Timer started");
 
     return () => {
-      // 버튼을 클릭하면 showTimer가 false로 변경되어 Timer 컴포넌트가 화면에서 제거되면서 return의 내부코드가 실행되며 타이머가 정리
+      // 정리 작업은 리소스를 해제하거나 이벤트를 제거하는 데 사용
+      // app4.jsx에 있는 버튼을 클릭하면 showTimer가 false로 변경되어 Timer 컴포넌트가 화면에서 제거되면서 return의 내부코드가 실행되며 타이머가 정리
       clearInterval(interval); // setInterval() 제거하는 함수
       console.log("Timer cleared");
     };
